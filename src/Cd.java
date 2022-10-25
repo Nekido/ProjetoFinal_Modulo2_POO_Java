a6d29555433d826d87b0a07cb792bb0c745adbd1
Caterine Studzinski <89097644+Nekido@users.noreply.github.com>
2022-10-25 17:20:05 -0300

    Projeto final POO Modulo2 programa DEVA - JAVA

---
+++ src/Cd.java
@@ -0,0 +1,18 @@
+public class Cd extends Midia{
+
+}
+
+
+
+//
+//    Criar classe Filha chamada CD
+//
+//        Atributos:
+//
+//        artista : String
+//        quantidadeFaixas : int
+//        Métodos
+//
+//        sobrescrever darPlay(): exibir a mensagem "Primeira faixa tocando"
+//        construtor(todos os argumentos)
+//        get() e set()
---
+++ src/DoacaoMidia.java
@@ -0,0 +1,17 @@
+public class DoacaoMidia {
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
+//    Criar classe DoacaoMidia
+//
+//        Essa classe deve conter método pegarMidiaAleatoria() que devolve uma midia já criada na biblioteca
+//
+//        aleatória
+//
+//        Esse classe deve conter método assisitirFilme(Dvd dvd) e chamar os dois métodos de legenda
+
+//        Método main: O main deve pegar uma midia aleatoria e dar o play
