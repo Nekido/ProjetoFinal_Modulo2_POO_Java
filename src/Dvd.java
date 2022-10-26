98ba33412e527be3d3895d629928f62c809bfe78
Laís Zanardi <89917698+zanardiL@users.noreply.github.com>
2022-10-26 10:23:48 -0300

    Atualização da classe Dvd;

--- src/Dvd.java
+++ src/Dvd.java
@@ -1,19 +1,63 @@
-public class Dvd extends Midia{
+public class Dvd extends Midia {
+    private String idioma;
+    private boolean possuiLegenda;
+    private String idiomaLegenda;
 
+    public Dvd(
+            String nome,
+            String codigo,
+            Double preco,
+            String idioma,
+            boolean possuiLegenda,
+            String idiomaLegenda
+    ) {
+        super(nome, codigo, preco);
+        this.idioma = idioma;
+        this.possuiLegenda = possuiLegenda;
+        this.idiomaLegenda = idiomaLegenda;
+    }
+
+    public String getIdioma() {
+        return idioma;
+    }
+
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+
+    public boolean isPossuiLegenda() {
+        return possuiLegenda;
+    }
+
+    public void setPossuiLegenda(boolean possuiLegenda) {
+        this.possuiLegenda = possuiLegenda;
+    }
+
+    public String getIdiomaLegenda() {
+        return idiomaLegenda;
+    }
+
+    public void setIdiomaLegenda(String idiomaLegenda) {
+        this.idiomaLegenda = idiomaLegenda;
+    }
+
+    @Override
+    public void darPlay() {
+        System.out.println("Filme iniciado");
+    }
+
+    public void legenda(boolean ligar, String idioma) {
+        if(isPossuiLegenda()) {
+            ligar = true;
+            this.idiomaLegenda = idioma;
+        }
+    }
+
+    public void legenda(boolean ligarDesligar) {
+        if(ligarDesligar) {
+            ligarDesligar = false;
+        } else {
+            ligarDesligar = true;
+        }
+    }
 }
-
-
-//    Criar classe Filha chamada Dvd
-//        Atributos:
-//
-//        idioma : String
-//        possuiLegenda : boolean
-//        idiomaLegenda : String
-//        Métodos
-//
-//        sobrescrever darPlay(): exibir a mensagem "filme iniciado"
-//        construtor(todos os argumentos)
-//        get() e set()
-//        legenda(boolean ligar, String idioma) : liga a legenda e atualiza o idioma da legenda
-//        legenda(boolean ligarDesligar) : apenas atualiza a variável possuiLegenda
-
