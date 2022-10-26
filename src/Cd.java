7c43d4d7979587e8b145e00606dee8a920c301bd
Caterine Studzinski <89097644+Nekido@users.noreply.github.com>
2022-10-25 21:01:35 -0300

    Adições corpo das classes

--- src/Cd.java
+++ src/Cd.java
@@ -1,5 +1,37 @@
 public class Cd extends Midia{
 
+    private String artista;
+    private  int quantidadeDeFaixas;
+
+    public Cd (String nome, String codigo, Double preco){
+        super(nome, codigo, preco);
+        this.artista = artista;
+        this.quantidadeDeFaixas = quantidadeDeFaixas;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public void setArtista(String artista ) {
+        this.artista = artista;
+    }
+
+    public int getQuantidadeDeFaixas() {
+        return quantidadeDeFaixas;
+    }
+
+    public void setQuantidadeDeFaixas(int quantidadeDeFaixas) {
+        this.quantidadeDeFaixas = quantidadeDeFaixas;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "Cd{" +
+                "artista='" + artista + '\'' +
+                ", quantidadeDeFaixas=" + quantidadeDeFaixas +
+                '}';
+    }
 }
 
 
--- src/Midia.java
+++ src/Midia.java
@@ -1,5 +1,52 @@
 public class Midia {
 
+    private String nome;
+    private String codigo;
+    private Double preco;
+
+    public Midia (String nome, String codigo, Double preco){
+        this.nome = nome;
+        this.codigo = codigo;
+        this.preco = preco;
+    }
+
+    public void darPlay() {
+        //desenvolver
+
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCodigo(){
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public Double getPreco(){
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    @Override
+    public String toString() {
+        return "Midia{" +
+                "nome='" + nome + '\'' +
+                ", codigo='" + codigo + '\'' +
+                ", preco=" + preco +
+                '}';
+    }
 }
 
 
