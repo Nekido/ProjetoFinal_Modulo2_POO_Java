c969d8a007e343e2378a27916c03770259d99791
Caterine Studzinski <89097644+Nekido@users.noreply.github.com>
2022-10-27 18:39:53 -0300

    criação metodos na classe DoacaoDeMidia.

--- src/DoacaoMidia.java
+++ src/DoacaoMidia.java
@@ -5,23 +5,24 @@ public class DoacaoMidia {
     public static void main(String[] args) {
 
         Midia midia = pegarMidiaAleatoria();
-
+        System.out.println(midia);
+        midia.darPlay();
 
     }
 
-
     public static Midia pegarMidiaAleatoria() {
-        Cd  um = new Cd("Reliquias", "25B369", 35.00,"Roberto Carlos",16);
-        Dvd  dois = new Dvd("Dupla Dinâmica","31MV55",66.87,"En-USA",true, "Pt-Br");
-        Cd tres= new Cd("Melhores do ano","02M378", 90.33,"Pagodão", 24);
-        Dvd quatro = new Dvd("Tom e Jerry","78ZY55", 21.00, "En-USA",true,"ES");
 
-        Random pegar= new Random();
+        Cd um = new Cd("Reliquias", "25B369", 35.00, "Roberto Carlos", 16);
+        Dvd dois = new Dvd("Dupla Dinâmica", "31MV55", 66.87, "En-USA", true, "Pt-Br");
+        Cd tres = new Cd("Melhores do ano", "02M378", 90.33, "Pagodão", 24);
+        Dvd quatro = new Dvd("Tom e Jerry", "78ZY55", 21.00, "En-USA", true, "ES");
+
+        Random pegar = new Random();
 
         int escolha = pegar.nextInt(3);
 
 
-        switch (escolha){
+        switch (escolha) {
 
             case 0:
                 return um;
@@ -37,9 +38,9 @@ public class DoacaoMidia {
 
     }
 
-public static void assitirfilme(){
-      //  Dvd dvd = new legenda();
-}
+    public static void assitirfilme() {
+        //  Dvd dvd = new legenda();
+    }
 }
 //    Criar classe DoacaoMidia
 //
